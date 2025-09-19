133551d3bf1091fce262b9212871e2159721be57
wwh <2287060518@qq.com>
2025-09-19 16:30:55 +0800

    add smoke_test

--- csrc/uvm_verdi_dpi.d
+++
@@ -1,8 +0,0 @@
-uvm_verdi_dpi.o: \
- /home/wwh/vcs/vcs2018/vcs/O-2018.09-SP2/etc/uvm-1.2/verdi/dpi/uvm_verdi_dpi.cpp \
- /home/wwh/vcs/vcs2018/vcs/O-2018.09-SP2/include/vpi_user.h \
- /home/wwh/vcs/vcs2018/vcs/O-2018.09-SP2/include/veriuser.h \
- /home/wwh/vcs/vcs2018/vcs/O-2018.09-SP2/include/sv_vpi_user.h \
- /home/wwh/vcs/vcs2018/vcs/O-2018.09-SP2/include/vpi_user.h \
- /home/wwh/vcs/vcs2018/vcs/O-2018.09-SP2/include/vcs_vpi_user.h \
- /home/wwh/vcs/vcs2018/vcs/O-2018.09-SP2/include/sv_vpi_user.h
